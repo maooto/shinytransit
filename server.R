893af9053ca53cf55cea2fa43fdd21254cc001ac
Matt Clark <maooto@gmail.com>
2017-10-26 13:32:44 -0700

    back to functional

--- server.R
+++ server.R
@@ -3,7 +3,7 @@
 
 shinyServer(function(input, output) { 
   
-  # ## INVOKE LEAFLET PLUGINS #############
+  ## INVOKE LEAFLET PLUGINS #############
   # polylineoffset_Plugin <- htmlDependency("Leaflet.PolylineOffset", "1.0.1",
   #                                         src = normalizePath(path = 'C:/users/matt clark/documents/R/win-library/3.4/Leaflet Plugins/Leaflet.PolylineOffset-master'),
   #                                         script = "leaflet.polylineoffset.js"
@@ -14,11 +14,7 @@ shinyServer(function(input, output) {
   #   map
   # }
 
-  
-  
-  
-  
-  
+
   ## DEFINE FUNCTIONS #############
   
   choosebasemap <- function(period) { 
@@ -70,18 +66,23 @@ shinyServer(function(input, output) {
   
   spldfmaker <- function(citychoice, odchoice, timechoice) { 
     
+    #subset the spatial dataframe for plotting, given user inputs 
+    
     od <- odhash[, c('corrcode', as.character(odchoice))]
     
     od1 <- od[od[,as.character(odchoice)] == as.character(citychoice), ]
     
-    res1 <- merge(res, od1, by = 'corrcode')
-    res2 <- res1[res1$period == as.character(timechoice), ]
     
     if (as.character(timechoice) == 'morning') { 
       spldf <- spldf.mor
+      res1 <- merge(res, od1, by = 'corrcode')
+      
     } else { 
       spldf <- spldf.eve  
+      res1 <- merge(reseve, od1, by = 'corrcode')
     }
+    
+    res2 <- res1[res1$period == as.character(timechoice), ]
     
     spldf <- spldf[spldf@data$corrcode%in% res2$corrcode, ]
     
@@ -94,30 +95,40 @@ shinyServer(function(input, output) {
   
   output$transitmap <- renderLeaflet({
     leaflet() %>% 
-      setView(lat = initlat, lng = initlong, zoom = initzoom)  
+      setView(lat = initlat, lng = initlong, zoom = initzoom) %>% 
+      addPolylines(data = spldf.mor, fillOpacity =  1, 
+                   stroke = T, 
+                   weight = 5, 
+                   color = linecolormaker(pmchoice = input$pmetric, timechoice = input$moreve))
     })
   
   ## OBSERVER FOR MAPPING ui VARIABLES ###########
   
   observe ({
 
-    spldf <<- spldfmaker(citychoice = as.character(input$city),
+    spldf <- spldfmaker(citychoice = as.character(input$city),
                              odchoice = as.character(input$od),
                              timechoice = as.character(input$moreve))
+    
+    
 
     leafletProxy("transitmap") %>%
       clearShapes() %>%
       addProviderTiles(choosebasemap(input$moreve)) %>%
-      addPolylines(data = spldf.mor, fillOpacity =  1,
-                   color = c('red', 'blue', 'yellow')) %>% #linecolormaker(pmchoice = input$pmetric, timechoice = input$moreve)) %>%
+      addPolylines(data = spldf, 
+                   fillOpacity =  1,
+                   stroke = T, 
+                   weight = 5, 
+                   color = linecolormaker(pmchoice = input$pmetric, timechoice = input$moreve), 
+                   highlightOptions = highlightOptions(stroke = T, 
+                                                       color = 'black', 
+                                                       weight = 2, 
+                                                       bringToFront = T)) %>%
       addLegend('bottomleft', colors = colordf$linecolor,
                 labels = colordf$bounds,
-                title = 'Title', #paste('Average daily peak-period ', as.character(input$pmetric), sep = ''),
-                layerId = 'legend') %>%
-      addPolylines(data = spldf, fillOpacity =  1,
-                   color = linecolormaker(pmchoice = input$pmetric, timechoice = input$moreve))
-      
-  })
+                title = paste('Average daily peak-period ', as.character(input$pmetric), sep = ''),
+                layerId = 'legend') 
+    })
 
   
   
